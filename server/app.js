4e2d8cff8a336b5d9670537b643dcb637aece322
Amy Venturino <aventuri@umich.edu>
2017-05-13 17:14:25 -0500

    Created fav db and favs adding on click, started angular routes

--- server/app.js
+++ server/app.js
@@ -8,6 +8,19 @@ var path = require('path');
 // globals
 var port = process.env.PORT || 3456;
 
+//create mongodb
+mongoose.connect( 'localhost:27017/WkndChal5' );
+
+//create mongoose Schema
+var movieSchema = mongoose.Schema({
+  title: String,
+  year: Number,
+  poster: String,
+  imdbID: String
+}); // end movieSchema
+
+var movies = mongoose.model( 'movies', movieSchema );
+
 //uses
 app.use(express.static(path.resolve('./server/public')));
 app.use( bodyParser.urlencoded( { extended: true } ) );
@@ -15,7 +28,16 @@ app.use( bodyParser.json() );
 
 app.get( '/', function( req, res ){
   res.sendFile( path.resolve( './server/public/views/index.html' ) );
-});
+});//end base url
+
+
+app.post('/favorites', function ( req, res ){
+  console.log(req.body);
+  var addMov = movies(req.body);
+  addMov.save().then(function(){
+    res.sendStatus( 200 );
+  });//end save
+});// end post
 
 
 app.listen( port, function() {
--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,4 +1,8 @@
-var myApp = angular.module('myApp', []);
+var myApp = angular.module('myApp',  ['ngRoute']);
+
+myApp.config(function($routeProvider){
+  
+})
 
 myApp.controller('SearchMov', function ($http, GetMov ){
   console.log('NG');
@@ -10,7 +14,23 @@ myApp.controller('SearchMov', function ($http, GetMov ){
       console.log('in .then return: ', data);
       vm.result = data;
     });//end .then funct
-  };
-//end submit funciton
+  };//end submit funciton
+
+  vm.addFav = function (y) {
+    console.log(y);
+    var movObj = {
+      title: y.Title,
+      year: y.Year,
+      poster: y.Poster,
+      imdbID: y.imdbID
+    };
+    $http({
+      method: 'POST',
+      url: '/favorites',
+      data: movObj
+    }).then( function ( response ) {
+      console.log( response );
+    });//end success
+  };//end addFave
 
 });//end controller
